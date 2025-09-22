63292df6620cda6969d91afab62000f458b1afb0
Deshan <90233631+DeshanGitHub@users.noreply.github.com>
2025-09-22 16:44:44 +0530

    Add text input and return value handling between screens
    Updated Home to handle values returned from Screen and print appropriate messages. Modified Screen to include a TextField and return its value when navigating back using the app bar back button.

--- flutter_projects/lib/home.dart
+++ flutter_projects/lib/home.dart
@@ -12,37 +12,50 @@ class Home extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              String? screenValue = await Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => Screen(tittle: 'Page One'),
                   ));
+              _checkScreenValue(screenValue: screenValue);
             },
             child: const Text('Go to Page One'),
           ),
           ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              String? screenValue = await Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => Screen(tittle: 'Page Two'),
                   ));
+              _checkScreenValue(screenValue: screenValue);
             },
             child: const Text('Go to Page Two'),
           ),
           ElevatedButton(
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              String? screenValue = await Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => Screen(tittle: 'Page Three'),
                   ));
+              _checkScreenValue(screenValue: screenValue);
             },
             child: const Text('Go to Page Three'),
           ),
         ],
       )),
     );
+  }
+
+  void _checkScreenValue({String? screenValue}) {
+    if (screenValue != null) {
+      screenValue.isEmpty
+          ? print('Value is empty')
+          : print('Value from Screen: $screenValue');
+    } else if (screenValue == null) {
+      print('No value returned from Screen');
+    }
   }
 }
--- flutter_projects/lib/screen.dart
+++ flutter_projects/lib/screen.dart
@@ -2,20 +2,35 @@ import 'package:flutter/material.dart';
 
 class Screen extends StatelessWidget {
   final String tittle;
+  final TextEditingController controller = TextEditingController();
 
-  const Screen({super.key,required this.tittle});
+  Screen({super.key, required this.tittle});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(tittle),
-      ),
-      body: Center(
-        child: Text(
-          'This is $tittle.',
-          style: const TextStyle(fontSize: 24),
+        leading: BackButton(
+          onPressed: () {
+            Navigator.pop(context, controller.text);
+          },
         ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Text(
+            'This is $tittle.',
+            style: const TextStyle(fontSize: 24),
+          ),
+          TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              labelText: 'Enter something',
+            ),
+          ),
+        ],
       ),
     );
   }
